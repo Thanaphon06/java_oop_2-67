8fe31f8968af7ba3b7f565addd4eb464775d5147
Thanaphon06 <tatahahaha007@gmail.com>
2024-11-28 09:59:32 +0700

    test1

--- week1/world.java
+++ week1/world.java
@@ -3,9 +3,19 @@ public class world {
     public static void main(String args[])
     {
         
-        System.out.println("welcome to the internet cafe");
+        /*System.out.println("welcome to the internet cafe");
         System.out.print("welcome to the internet cafe");
         System.out.println( "Welcome\nto\nJava\nProgramming!" );
+        */
         // \n backslsah n (\n) คือการเว้นบรรทัด
+
+        System.out.println( 123 );
+ System.out.println( 123L ); //เปลี่ยนจากตัวอักษณ เป็น long
+ System.out.println( 0123 );  //0 คือการแปลงจากฐาน 8 ไป 10
+ System.out.println( 0x123 ); //0xน่าจะเปลงเป็น เลขฐาน16 ไป 10
+ System.out.println( 123.50 );
+ System.out.println( 123.5f ); //f คือ เปลี่ยนเป็น float
+ System.out.println( 123e3 );
+ System.out.println( 123e-2 ); //e คือการยกกำลัง
     }
 }
