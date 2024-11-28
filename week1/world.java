b5b01c3276b8bafca0425379ed9b085988af88a6
Thanaphon06 <tatahahaha007@gmail.com>
2024-11-28 09:34:39 +0700

    first week test

---
+++ week1/world.java
@@ -0,0 +1,11 @@
+public class world {
+
+    public static void main(String args[])
+    {
+        
+        System.out.println("welcome to the internet cafe");
+        System.out.print("welcome to the internet cafe");
+        System.out.println( "Welcome\nto\nJava\nProgramming!" );
+        // \n backslsah n (\n) คือการเว้นบรรทัด
+    }
+}
